eeb764d760a340190e52eaa72d98d948f7780f24
Denisse Lara <dmlaramartin@gmail.com>
2022-01-03 20:05:44 -0400

    Remueve método create no utilizado en Transactions.
    Agrega comentarios javadoc a Transactions.

--- src/main/java/com/finanzaspersonales/model/Transactions.java
+++ src/main/java/com/finanzaspersonales/model/Transactions.java
@@ -4,14 +4,20 @@ import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
 
+/**
+ * Performs all the transactions DB operations.
+ * @author denisse
+ * @version 1.0
+ * @since 1.0
+ */
 public class Transactions {
 
   private Transactions() {}
 
-  public static void create(Transaction transaction) {
-    Database.db().saveTransaction(transaction);
-  }
-
+  /**
+   * Creates a new transaction using the provided values.
+   * @return A new transaction instance.
+   */
   @NotNull
   public static Transaction create(@NotNull Transaction.TransactionType type, Category category,
                                    LocalDate date, double amount, String description) {
@@ -26,30 +32,54 @@ public class Transactions {
     return transaction;
   }
 
+  /**
+   * Updates an existing transaction using the values inside the provided instance.
+   * @param transaction Transaction with an existing ID.
+   */
   public static void update(Transaction transaction) {
     Database.db().updateTransaction(transaction);
   }
 
+  /**
+   * @return All transactions with date equal to LocalDate.now()
+   */
   public static Transaction[] getToday() {
     return Database.db().getTransactionsByDate(LocalDate.now());
   }
 
+  /**
+   * @return All recorded transactions.
+   */
   public static Transaction[] getAll() {
     return Database.db().getAllTransactions();
   }
 
+  /**
+   * @param id Existing ID.
+   * @return Matching transaction.
+   */
   public static Transaction getByID(String id) {
     return Database.db().getTransactionByID(id);
   }
 
+  /**
+   * Verifies if a transaction exists.
+   */
   public static boolean exists(String id) {
     return Database.db().transactionExists(id);
   }
 
+  /**
+   * Deletes a transaction from the database.
+   * @param id Existing ID.
+   */
   public static void delete(String id) {
     Database.db().deleteTransaction(id);
   }
 
+  /**
+   * Deletes all transactions from the database.
+   */
   public static void deleteAll() {
     Database.db().deleteAllTransactions();
   }
